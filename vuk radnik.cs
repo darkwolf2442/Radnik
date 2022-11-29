3f20a4c810940bd28a32465b7b74440349335f76
darkwolf2442 <119406841+darkwolf2442@users.noreply.github.com>
2022-11-29 17:47:44 +0100

    Add files via upload

---
+++ vuk radnik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public abstract class Radnik
+    {
+        private string ime;
+        private double procenat;
+        protected Radnik(string i, double p) 
+        {
+            ime = i;
+            procenat = p;
+        }
+        public abstract double Prihod { get; }
+        public double Plata { get { return Prihod * procenat / 100; } }
+        public override string ToString()
+        {
+            return ime + "/"+ Plata;
+        }
+    }
+}
